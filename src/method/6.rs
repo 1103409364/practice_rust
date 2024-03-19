02decbef4e3e8118f487a73779e96132f764f27c
wcs <1103409364@qq.com>
2024-03-19 13:55:27 +0800

    method
    Signed-off-by: wcs <1103409364@qq.com>

--- src/method/6.rs
+++ src/method/6.rs
@@ -9,8 +9,12 @@ enum TrafficLightColor {
 
 // 为 TrafficLightColor 实现所需的方法
 impl TrafficLightColor {
-    fn color(&self) -> &str {
-        "yellow"
+    fn color(&self) ->String {
+        match *self {
+            TrafficLightColor::Red => "red".to_string(),
+            TrafficLightColor::Yellow => "yellow".to_string(),
+            TrafficLightColor::Green => "green".to_string(),
+        }
     }
 }
 
