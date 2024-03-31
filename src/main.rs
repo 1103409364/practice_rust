6b7745bfc6fc9d97b314ccaec66a465c0cf44a96
wcs <1103409364@qq.com>
2024-03-31 15:03:32 +0800

    chore: use stderr
    Signed-off-by: wcs <1103409364@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,18 @@ fn main() {
     // let config = Config::from(&args)?; //  这种方式，需要标出 main 的返回值 -> Result<(), Box<dyn Error>>。错误向上传播，没有处理
     // unwrap_or_else 是定义在 Result<T,E> 上的常用方法，如果 Result 是 Ok，那该方法就类似 unwrap：返回 Ok 内部的值；如果是 Err，就调用闭包中的自定义代码对错误进行进一步处理
     let config = Config::from(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         // process::exit(1) 来终结进程，其中 1 是一个信号值(事实上非 0 值都可以)，通知调用我们程序的进程，程序是因为错误而退出的。
         process::exit(1);
     });
 
-    println!(
+    eprintln!(
         "Searching for {}, In file {}",
         config.query, config.file_path
     );
     // if let 模式匹配 Err(e) 匹配到错误
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
