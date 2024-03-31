c91371de02b3e00c47212834f0c3190fc8340ccc
wcs <1103409364@qq.com>
2024-03-31 12:06:15 +0800

    refactor: use Result
    Signed-off-by: wcs <1103409364@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg stands for debug print。dbg 是 Rust 标准库中的一个宏，可以打印变量的值
     // env::args 读取到的参数中第一个就是程序的可执行路径名。
     // dbg!(args);
-    let config = Config::new(&args);
+    // 对 build 返回的 `Result` 进行处理
+    // let config = Config::from(&args)?; //  这种方式，需要标出 main 的返回值 -> Result<(), Box<dyn Error>>。错误向上传播，没有处理
+    let config = Config::from(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -23,9 +29,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    // 从代码惯例的角度出发，new 往往不会失败，毕竟新建一个实例没道理失败，因此修改为 from 会更加合适
+    fn from(args: &[String]) -> Result<Self, &'static str> {
+        // 缺少参数报错，防止数组越界
+        if args.len() < 3 {
+            // panic!("not enough arguments")
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Self { query, file_path }
+
+        Ok(Self { query, file_path })
     }
 }
