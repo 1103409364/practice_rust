f51d04b4f094f0c43b1d009a5b30a1fbc3f02488
wcs <1103409364@qq.com>
2024-03-31 14:45:55 +0800

    feat: search_case_insensitive
    Signed-off-by: wcs <1103409364@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use std::env;
 use std::error::Error;
-use std::{fs, vec};
-
+use std::fs;
 // Config 中存储的并不是 &str 这样的引用类型，而是一个 String 字符串，也就是 Config 并没有去借用外部的字符串，而是拥有内部字符串的所有权。
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -15,18 +16,32 @@ impl Config {
             // panic!("not enough arguments")
             return Err("not enough arguments");
         }
-
+        // 获取终端传入参数
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Self { query, file_path })
+        // 从环境变量中读取配置，是否忽略大小写
+        // is_ok 是 Result 的方法，用于检查是否有值。返回 true/false。这里不关心 Ok<T> 中的具体值。
+        // $ IGNORE_CASE=1 cargo run -- to file.txt
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Self {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    // println!("with text:\n{contents}");
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}")
     }
     Ok(())
@@ -45,6 +60,21 @@ safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
 // 返回引用类型需要标出 lifetime
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
@@ -57,5 +87,19 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     }
 
     // return results;
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // to_lowercase 返回的是 String。
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
     results
 }
