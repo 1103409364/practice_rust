38c023b5032aa761008c1ff379141714a14a15b6
1103409364 <11034093654@qq.com>
2024-11-02 13:28:27 +0800

    comment
    Signed-off-by: 1103409364 <11034093654@qq.com>

--- src/demo/char_length.rs
+++ src/demo/char_length.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // String 的 len() 方法返回的是字符串中字节的长度，
+    // String 的 len() 方法返回的是字符串中【字节】的长度，
     // 而 chars().count() 方法返回的是字符串中 Unicode 字符的个数。
     let message = "1二三四五六七八九十";
     let byte_length = message.len();
