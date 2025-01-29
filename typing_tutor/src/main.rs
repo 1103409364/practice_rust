a126db85477073ec670e9373788c67c4fa634261
15x <1103409364@email.com>
2025-01-29 13:18:50 +0800

    feat: add support for special characters in user input handling
    Signed-off-by: 15x <1103409364@email.com>

--- typing_tutor/src/main.rs
+++ typing_tutor/src/main.rs
@@ -19,6 +19,15 @@ impl App {
             user_input: String::new(),
         })
     }
+    // push 特殊字符
+    fn push_char(&mut self, c: char) {
+        let char_len = self.file_content.chars().count();
+
+        // 限制输入长度
+        if self.user_input.chars().count() < char_len {
+            self.user_input.push(c);
+        };
+    }
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -43,10 +52,118 @@ fn main() -> Result<(), std::io::Error> {
                     }
                     KeyCode::Esc => break,
                     KeyCode::Char(c) => {
-                        let char_len = app.file_content.chars().count();
-                        // 限制输入长度
-                        if app.user_input.chars().count() < char_len {
-                            app.user_input.push(c);
+                        // 处理特殊字符，例如重音符号
+                        if ['\'', '`', '^', '~', '"'].contains(&c) {
+                            let pre_char = app.user_input.pop();
+                            match c {
+                                '\'' => {
+                                    if let Some(pre_char) = pre_char {
+                                        match pre_char {
+                                            'a' => app.push_char('á'),
+                                            'e' => app.push_char('é'),
+                                            'i' => app.push_char('í'),
+                                            'o' => app.push_char('ó'),
+                                            'u' => app.push_char('ú'),
+                                            'A' => app.push_char('Á'),
+                                            'E' => app.push_char('É'),
+                                            'I' => app.push_char('Í'),
+                                            'O' => app.push_char('Ó'),
+                                            'U' => app.push_char('Ú'),
+                                            _ => {
+                                                app.push_char(pre_char);
+                                                app.push_char(c);
+                                            }
+                                        }
+                                    } else {
+                                        app.push_char(c);
+                                    }
+                                }
+                                '`' => {
+                                    if let Some(pre_char) = pre_char {
+                                        match pre_char {
+                                            'a' => app.push_char('à'),
+                                            'e' => app.push_char('è'),
+                                            'i' => app.push_char('ì'),
+                                            'o' => app.push_char('ò'),
+                                            'u' => app.push_char('ù'),
+                                            'A' => app.push_char('À'),
+                                            'E' => app.push_char('È'),
+                                            'I' => app.push_char('Ì'),
+                                            'O' => app.push_char('Ò'),
+                                            'U' => app.push_char('Ù'),
+                                            _ => {
+                                                app.push_char(pre_char);
+                                                app.push_char(c);
+                                            }
+                                        }
+                                    } else {
+                                        app.push_char(c);
+                                    }
+                                }
+                                '^' => {
+                                    if let Some(pre_char) = pre_char {
+                                        match pre_char {
+                                            'a' => app.push_char('â'),
+                                            'e' => app.push_char('ê'),
+                                            'i' => app.push_char('î'),
+                                            'o' => app.push_char('ô'),
+                                            'u' => app.push_char('û'),
+                                            'A' => app.push_char('Â'),
+                                            'E' => app.push_char('Ê'),
+                                            'I' => app.push_char('Î'),
+                                            'O' => app.push_char('Ô'),
+                                            'U' => app.push_char('Û'),
+                                            _ => {
+                                                app.push_char(pre_char);
+                                                app.push_char(c);
+                                            }
+                                        }
+                                    }
+                                }
+                                '~' => {
+                                    if let Some(pre_char) = pre_char {
+                                        match pre_char {
+                                            'a' => app.push_char('ã'),
+                                            'n' => app.push_char('ñ'),
+                                            'o' => app.push_char('õ'),
+                                            'A' => app.push_char('Ã'),
+                                            'N' => app.push_char('Ñ'),
+                                            'O' => app.push_char('Õ'),
+                                            _ => {
+                                                app.push_char(pre_char);
+                                                app.push_char(c);
+                                            }
+                                        }
+                                    }
+                                }
+                                '"' => {
+                                    if let Some(pre_char) = pre_char {
+                                        match pre_char {
+                                            'a' => app.push_char('ä'),
+                                            'e' => app.push_char('ë'),
+                                            'i' => app.push_char('ï'),
+                                            'o' => app.push_char('ö'),
+                                            'u' => app.push_char('ü'),
+                                            'y' => app.push_char('ÿ'),
+                                            'A' => app.push_char('Ä'),
+                                            'E' => app.push_char('Ë'),
+                                            'I' => app.push_char('Ï'),
+                                            'O' => app.push_char('Ö'),
+                                            'U' => app.push_char('Ü'),
+                                            'Y' => app.push_char('Ÿ'),
+                                            _ => {
+                                                app.push_char(pre_char);
+                                                app.push_char(c);
+                                            }
+                                        }
+                                    }
+                                }
+                                _ => {
+                                    app.push_char(c);
+                                }
+                            }
+                        } else {
+                            app.push_char(c);
                         }
                     }
                     KeyCode::Enter => {
