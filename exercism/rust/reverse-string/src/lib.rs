3576a1bd84897f29aff8b41a3cdc280f336677a3
15x <1103409364@email.com>
2025-02-12 23:55:02 +0800

    refactor: 将字符串反转的实现简化为一行代码
    Signed-off-by: 15x <1103409364@email.com>

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,8 +1,4 @@
 pub fn reverse(input: &str) -> String {
-    // todo!("Write a function to reverse {input}");
-    let mut reversed = String::new();
-    for c in input.chars().rev() {
-        reversed.push(c);
-    }
-    reversed
+    // .collect(): 迭代器方法，它将迭代器中的所有元素收集到一个新的集合中。在这种情况下，因为返回类型是 String，collect() 会将反转后的字符序列组合成一个新的 String。
+    input.chars().rev().collect()
 }
