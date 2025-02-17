aa23ec1e55d1a067d940225e8e10ca787552ebcf
15x <1103409364@email.com>
2025-02-17 23:26:44 +0800

    fix: 修复线程练习中的同步和消息传递问题
    Signed-off-by: 15x <1103409364@email.com>

--- rustlings/exercises/20_threads/threads3.rs
+++ rustlings/exercises/20_threads/threads3.rs
@@ -1,5 +1,5 @@
 use std::{sync::mpsc, thread, time::Duration};
-
+// std::sync::mpsc 是 Rust 标准库中提供的一个模块，用于实现多生产者单消费者（Multiple Producer, Single Consumer）的异步消息传递通道。  它的主要作用是在并发的线程或异步任务之间安全地传递数据。
 struct Queue {
     first_half: Vec<u32>,
     second_half: Vec<u32>,
@@ -17,10 +17,12 @@ impl Queue {
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     // TODO: We want to send `tx` to both threads. But currently, it is moved
     // into the first thread. How could you solve this problem?
+    // 克隆发送者（tx），每个线程都需要一个独立的发送者
+    let txc = tx.clone();
     thread::spawn(move || {
         for val in q.first_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            txc.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
