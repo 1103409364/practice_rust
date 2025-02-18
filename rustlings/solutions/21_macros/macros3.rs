8854a7454533f4065f3a768a3b4427029ec54c34
15x <1103409364@email.com>
2025-02-18 21:34:24 +0800

    fix: 修复宏调用和定义问题
    Signed-off-by: 15x <1103409364@email.com>

--- rustlings/solutions/21_macros/macros3.rs
+++ rustlings/solutions/21_macros/macros3.rs
@@ -1,4 +1,14 @@
+// Added the attribute `macro_use` attribute.
+#[macro_use]
+mod macros {
+    // 或者使用 #[macro_export] #[macro_export]: 这个属性告诉 Rust 编译器，这个宏应该被导出，以便在其他模块中使用。如果没有这个属性，宏将只能在定义它的模块内部使用。
+    macro_rules! my_macro {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+}
+
 fn main() {
-    // DON'T EDIT THIS SOLUTION FILE!
-    // It will be automatically filled after you finish the exercise.
+    my_macro!();
 }
