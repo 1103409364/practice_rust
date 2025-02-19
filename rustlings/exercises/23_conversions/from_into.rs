4750a8eb2bb1f7d5b527b2c869677559261a30b7
15x <1103409364@email.com>
2025-02-19 22:41:53 +0800

    fix: 实现 `From<&str>` 和 `FromStr` 为 `Person` 结构体，并添加测试用例
    Signed-off-by: 15x <1103409364@email.com>

--- rustlings/exercises/23_conversions/from_into.rs
+++ rustlings/exercises/23_conversions/from_into.rs
@@ -34,7 +34,21 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let p: Vec<&str> = s.split(",").collect();
+        let default_person = Self::default();
+        if p.len() != 2 || p[0].is_empty() {
+            return default_person;
+        }
+
+        match p[1].parse::<u8>() {
+            Ok(age) => Person {
+                name: p[0].to_string(),
+                age,
+            },
+            Err(_) => default_person,
+        }
+    }
 }
 
 fn main() {
--- rustlings/exercises/23_conversions/from_str.rs
+++ rustlings/exercises/23_conversions/from_str.rs
@@ -41,7 +41,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(",");
+        let (Some(name), Some(age), None) = (split.next(), split.next(), split.next()) else {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        if name.is_empty() {
+            return Err(Self::Err::NoName);
+        };
+
+        match age.parse::<u8>() {
+            Ok(x) => Ok(Self {
+                name: name.to_string(),
+                age: x,
+            }),
+            Err(e) => Err(ParsePersonError::ParseInt(e)),
+        }
+    }
 }
 
 fn main() {
