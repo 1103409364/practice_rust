c6153ec1fad52594acf0689afafffbfdc31a5afc
Z7M <1103409364@qq.com>
2025-02-20 14:54:41 +0800

    refactor: 使用宏规则简化结构体和实现代码
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/space-age/src/lib.rs
+++ exercism/rust/space-age/src/lib.rs
@@ -11,25 +11,30 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+    fn years_during(d: &Duration) -> f64;
+}
+
+// 创建结构体，传入
+macro_rules! make_struct {
+    // $(...) 重复匹配 ident 标识符 * 匹配零个或多个 ident 标识符
+    ($($s_name:ident),*) => {
+        $(
+            pub struct $s_name;
+        )*
+    };
+}
+
+macro_rules! impl_planet {
+    // for 分隔符，自定义。ty 类型, + 一个或多个
+    (for $($t:ty),+) => {
+        $(impl Planet for $t {
+            fn years_during(d: &Duration) -> f64 {
+                todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+            }
+        })*
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+make_struct!(Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune);
+// for 占位，可以不要
+impl_planet!(for Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune);
