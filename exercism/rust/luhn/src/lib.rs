2a7bdfbb130a8ebc835034b1c7d2c01e9109d0eb
Z7M <1103409364@qq.com>
2025-02-24 14:14:58 +0800

    fix(luhn): 实现Luhn校验算法并激活测试用例
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/luhn/src/lib.rs
+++ exercism/rust/luhn/src/lib.rs
@@ -1,4 +1,24 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    // todo!("Is the Luhn checksum for {code} valid?");
+    let code_vec: Vec<i32> = code
+        .split("")
+        .filter(|&s| s != " " && s != "")
+        .map(|s| s.parse::<i32>().unwrap_or(-1))
+        .collect();
+
+    if code_vec.len() <= 1 || code_vec.iter().any(|n| *n == -1) {
+        return false;
+    }
+
+    let db_sum = code_vec
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acct, (i, n)| match i % 2 {
+            0 => acct + n,
+            _ => acct + if n * 2 > 9 { n * 2 - 9 } else { n * 2 },
+        });
+
+    db_sum % 10 == 0
 }
