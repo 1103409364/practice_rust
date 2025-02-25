50b047c95e594b126b04b6b0fd8fca65dcaa8740
Z7M <1103409364@qq.com>
2025-02-25 13:57:43 +0800

    fix: 阿姆斯特朗数判断功能
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    // todo!("true if {num} is an armstrong number")
+    let str_num = num.to_string();
+    let digits = str_num.len() as u32;
+
+    str_num.chars().fold(0, |acct, cur| {
+        // acct + cur.to_string().parse::<u32>().unwrap_or(0).pow(digits)
+        acct + cur.to_digit(10).unwrap_or(0).pow(digits)
+    }) == num
 }
