b3df9e0cce5d660566bb70331146c42c0a2fca45
Z7M <1103409364@qq.com>
2025-02-27 13:46:53 +0800

    fix: difference_of_squares
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/difference-of-squares/src/lib.rs
+++ exercism/rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,14 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    // todo!("square of sum of 1...{n}")
+    ((n + 1) * n / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    // todo!("sum of squares of 1...{n}")
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    // todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
