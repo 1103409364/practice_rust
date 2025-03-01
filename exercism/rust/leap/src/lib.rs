fd709848857761d141b6bdce8e29b43e466f3f36
15x <1103409364@email.com>
2025-03-01 10:26:10 +0800

    fix: is_leap_year
    Signed-off-by: 15x <1103409364@email.com>

--- exercism/rust/leap/src/lib.rs
+++ exercism/rust/leap/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_leap_year(year: u64) -> bool {
-    todo!("true if {year} is a leap year")
+    // todo!("true if {year} is a leap year")
+    match (year % 4 == 0, year % 100 != 0) {
+        (true, true) => true,
+        _ => match year % 400 == 0 {
+            true => true,
+            _ => false,
+        },
+    }
 }
