48f279495709c983e2cfd8ec831c51600c183522
Z7M <1103409364@qq.com>
2025-03-03 13:19:11 +0800

    fix: 实现质因数分解功能并启用所有测试
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,3 +1,17 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+pub fn factors(mut n: u64) -> Vec<u64> {
+    // todo!("This should calculate the prime factors of {n}")
+    let mut factors = Vec::new();
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            factors.push(i);
+            n /= i;
+        } else {
+            i += 1;
+        }
+    }
+    if n > 1 {
+        factors.push(n);
+    }
+    factors
 }
