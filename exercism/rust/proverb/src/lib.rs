b39219720c0f1adb57b245821fd982382bdfe243
Z7M <1103409364@qq.com>
2025-03-04 13:28:53 +0800

    fix: 完成谚语构建功能
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/proverb/src/lib.rs
+++ exercism/rust/proverb/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    // todo!("build a proverb from this list of items: {list:?}")
+    list.iter()
+        .enumerate()
+        .map(|(i, l)| {
+            if i == list.len() - 1 {
+                return format!("And all for the want of a {}.", list[0]);
+            }
+            format!("For want of a {} the {} was lost.", l, list[i + 1])
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
 }
