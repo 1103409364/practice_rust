4193b8b84a0fed7c5a2766dfc57be09b730c63e6
Z7M <1103409364@qq.com>
2025-03-05 14:23:21 +0800

    fix(raindrops): raindrops
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/raindrops/src/lib.rs
+++ exercism/rust/raindrops/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn raindrops(n: u32) -> String {
-    todo!("what sound does Raindrop #{n} make?")
+    // todo!("what sound does Raindrop #{n} make?")
+    let s = [3, 5, 7]
+        .map(|x| match x {
+            3 => match n % 3 {
+                0 => "Pling".to_string(),
+                _ => "".to_string(),
+            },
+            5 => match n % 5 {
+                0 => "Plang".to_string(),
+                _ => "".to_string(),
+            },
+            7 => match n % 7 {
+                0 => "Plong".to_string(),
+                _ => "".to_string(),
+            },
+            _ => "".to_string(),
+        })
+        .join("");
+
+    if s.is_empty() { n.to_string() } else { s }
 }
