4d5018f3934e80bbc11009ed7f11afc897f16460
Z7M <1103409364@qq.com>
2025-03-06 13:10:45 +0800

    fix: sum_of_multiples
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/sum-of-multiples/src/lib.rs
+++ exercism/rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    // todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    factors
+        .iter()
+        .fold(vec![], |mut acc, factor| {
+            let mut i = 1;
+            let mut x = *factor * i;
+            while x < limit && *factor != 0 {
+                if !acc.contains(&x) {
+                    acc.push(x)
+                }
+                i += 1;
+                x = *factor * i;
+            }
+            acc
+        })
+        .into_iter()
+        .sum()
 }
