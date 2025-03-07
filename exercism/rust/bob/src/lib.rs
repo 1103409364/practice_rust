788a2b076f41b29d2aa983048a31b15dae0ce6bb
Z7M <1103409364@qq.com>
2025-03-07 13:27:23 +0800

    fix: 修改回复逻辑以处理空消息情况
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/bob/src/lib.rs
+++ exercism/rust/bob/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn reply(mut message: &str) -> &str {
-    // todo!("have Bob reply to the incoming message: {message}")
     message = message.trim();
-    // 问号 大写 空，三种情况
+    // 三种情况，分类讨论：问号、大写、空
     match (
         message.ends_with("?"),
         message.to_uppercase() == message && !message.chars().all(|x| !x.is_alphabetic()),
-        message == "",
+        message.is_empty(),
     ) {
         (_, _, true) => "Fine. Be that way!",
         (true, true, false) => "Calm down, I know what I'm doing!",
