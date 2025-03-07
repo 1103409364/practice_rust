44ab8a6594f515bb08f3f351f902f188615eff65
Z7M <1103409364@qq.com>
2025-03-07 13:22:26 +0800

    fix: reply
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/bob/src/lib.rs
+++ exercism/rust/bob/src/lib.rs
@@ -1,3 +1,16 @@
-pub fn reply(message: &str) -> &str {
-    todo!("have Bob reply to the incoming message: {message}")
+pub fn reply(mut message: &str) -> &str {
+    // todo!("have Bob reply to the incoming message: {message}")
+    message = message.trim();
+    // 问号 大写 空，三种情况
+    match (
+        message.ends_with("?"),
+        message.to_uppercase() == message && !message.chars().all(|x| !x.is_alphabetic()),
+        message == "",
+    ) {
+        (_, _, true) => "Fine. Be that way!",
+        (true, true, false) => "Calm down, I know what I'm doing!",
+        (true, false, false) => "Sure.",
+        (false, true, false) => "Whoa, chill out!",
+        (false, false, false) => "Whatever.",
+    }
 }
