8722a29c12dd450af4d3a724eb621e10ee697b20
Z7M <1103409364@qq.com>
2025-03-13 14:43:30 +0800

    fix: 完成括号匹配功能并移除忽略标记
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/matching-brackets/src/lib.rs
+++ exercism/rust/matching-brackets/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    // todo!("Check if the string \"{string}\" contains balanced brackets");
+    const LEFT_BRACKETS: [char; 4] = ['(', '[', '{', '<'];
+    const RIGHT_BRACKETS: [char; 4] = [')', ']', '}', '>'];
+    let mut stack = vec![];
+    for c in string.chars() {
+        if LEFT_BRACKETS.contains(&c) {
+            stack.push(c);
+        }
+        if RIGHT_BRACKETS.contains(&c) {
+            if let Some(left) = stack.pop() {
+                if LEFT_BRACKETS.iter().position(|&x| x == left)
+                    != RIGHT_BRACKETS.iter().position(|&x| x == c)
+                {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    stack.is_empty()
 }
