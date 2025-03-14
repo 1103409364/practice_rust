34c905ed236218896925bfc89c53a0939ffc7255
Z7M <1103409364@qq.com>
2025-03-14 13:59:41 +0800

    refactor: 增加溢出保护
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/collatz-conjecture/src/lib.rs
+++ exercism/rust/collatz-conjecture/src/lib.rs
@@ -5,10 +5,10 @@ pub fn collatz(n: u64) -> Option<u64> {
         mut n => {
             while n != 1 {
                 count += 1;
-                match n % 2 {
-                    0 => n /= 2,
-                    _ => n = n * 3 + 1,
-                }
+                n = match n % 2 {
+                    0 => n / 2,
+                    _ => n.checked_mul(3)?.checked_add(1)?, // Add overflow protection
+                };
                 if count > 1000 {
                     return None;
                 }
