c4c8ce3c79bde86cd996a5fae6b36f13d3f2b811
Z7M <1103409364@qq.com>
2025-03-19 14:11:52 +0800

    docs: 添加 windows 方法的注释
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/series/src/lib.rs
+++ exercism/rust/series/src/lib.rs
@@ -2,7 +2,7 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
     digits
         .chars()
         .collect::<Vec<char>>()
-        .windows(len)
+        .windows(len) // 当 windows 方法的参数（即窗口大小 len）超过切片的最大长度时，windows 方法会返回一个空迭代器
         .map(|w| w.iter().collect())
         .collect()
 }
