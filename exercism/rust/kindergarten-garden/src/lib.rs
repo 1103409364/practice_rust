6535f1529d4a4bdc84b964c5dc07736f1c0be214
Z7M <1103409364@qq.com>
2025-03-20 14:11:22 +0800

    fix(kindergarten-garden): 实现根据学生和图谱返回植物的功能
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/kindergarten-garden/src/lib.rs
+++ exercism/rust/kindergarten-garden/src/lib.rs
@@ -1,3 +1,28 @@
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    todo!("based on the {diagram}, determine the plants the {student} is responsible for");
+    // Alice, Bob, Charlie, David, Eve, Fred, Ginny, Harriet, Ileana, Joseph, Kincaid, and Larry.
+    const STUDENTS: [&str; 12] = [
+        "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+        "Kincaid", "Larry",
+    ];
+
+    let index = STUDENTS.iter().position(|&s| s == student).unwrap();
+
+    diagram
+        .split_whitespace()
+        .enumerate()
+        .fold(vec![], |mut acc, (_, c)| {
+            acc.extend(
+                &c[index * 2..index * 2 + 2]
+                    .chars()
+                    .map(|c| match c.to_string().as_str() {
+                        "G" => "grass",
+                        "C" => "clover",
+                        "R" => "radishes",
+                        "V" => "violets",
+                        _ => panic!("Invalid character in diagram"),
+                    })
+                    .collect::<Vec<&str>>(),
+            );
+            acc
+        })
 }
