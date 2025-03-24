fc1e106f2302f4f1b2d73ffeb1b7318d06359adc
Z7M <1103409364@qq.com>
2025-03-24 14:13:54 +0800

    fix: 添加注释
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/acronym/src/lib.rs
+++ exercism/rust/acronym/src/lib.rs
@@ -1,9 +1,11 @@
 pub fn abbreviate(phrase: &str) -> String {
     phrase
+        // .split(|c: char| c.is_whitespace() || c == '-' || c == '_')
         .replace(['-', '_'], " ")
         .split_whitespace()
         // flat_map 接受一个闭包，该闭包将迭代器中的每个元素转换为一个新的迭代器，然后 flat_map 将这些新的迭代器连接成一个扁平的迭代器。
         .flat_map(|mut word| {
+            // word.chars().skip_while(|c| c.is_uppercase()) skip_while 接受一个闭包作为参数。这个闭包会被应用于迭代器中的每个元素，直到闭包返回 false 为止。跳过所有从起始位置开始的连续大写字母字符。
             if word == word.to_uppercase() {
                 word = &word[0..1];
             }
