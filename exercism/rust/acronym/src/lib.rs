3c843ff7a8807a45bfef90393b1959aab44bdd90
Z7M <1103409364@qq.com>
2025-03-24 14:06:01 +0800

    fix: 实现缩写功能并激活所有测试用例
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/acronym/src/lib.rs
+++ exercism/rust/acronym/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn abbreviate(phrase: &str) -> String {
-    todo!("Given the phrase '{phrase}', return its acronym");
+    phrase
+        .replace(['-', '_'], " ")
+        .split_whitespace()
+        // flat_map 接受一个闭包，该闭包将迭代器中的每个元素转换为一个新的迭代器，然后 flat_map 将这些新的迭代器连接成一个扁平的迭代器。
+        .flat_map(|mut word| {
+            if word == word.to_uppercase() {
+                word = &word[0..1];
+            }
+
+            word.chars()
+                .take(1) // take 返回 Take 是迭代器（实现了 Iterator trait）
+                .chain(word.chars().skip(1).filter(|c| c.is_uppercase()))
+        })
+        .map(|c| c.to_uppercase().to_string())
+        .collect()
 }
