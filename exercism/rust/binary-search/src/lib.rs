ac29f4f9a58132f50d43f92ca7beaed5cd901c0f
Z7M <1103409364@qq.com>
2025-04-10 14:22:07 +0800

    feat: refactor find function to accept generic key type and improve array handling
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,22 +1,31 @@
-pub fn find<T: Ord>(array: &[T], key: T) -> Option<usize> {
+use std::borrow::Borrow;
+
+pub fn find<T, K>(array: impl AsRef<[T]>, key: K) -> Option<usize>
+where
+    T: Ord,
+    K: Borrow<T>,
+{
+    let array = array.as_ref();
     if array.is_empty() {
         return None;
     }
+
     let mut left = 0;
     let mut right = array.len() - 1;
+
     while left <= right {
         let mid = (left + right) / 2;
-
-        match array[mid].cmp(&key) {
+        println!("left: {}, right: {}, mid: {}", left, right, mid);
+        match array[mid].cmp(key.borrow()) {
             std::cmp::Ordering::Equal => return Some(mid),
             std::cmp::Ordering::Less => {
-                if mid == 0 {
+                if mid == 0 && right == 0 {
                     return None;
                 }
                 left = mid + 1
             }
             std::cmp::Ordering::Greater => {
-                if mid == 0 {
+                if mid == 0 && left == 0 {
                     return None;
                 }
                 right = mid - 1
