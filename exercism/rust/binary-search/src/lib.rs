66c41027d1ee9b42d74b1e7144a44fac031d834c
Z7M <1103409364@qq.com>
2025-04-10 14:10:08 +0800

    feat: refactor find function to use generics and improve search logic
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,21 +1,26 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<T: Ord>(array: &[T], key: T) -> Option<usize> {
     if array.is_empty() {
         return None;
     }
     let mut left = 0;
     let mut right = array.len() - 1;
     while left <= right {
-        println!("left: {}, right: {}", left, right);
         let mid = (left + right) / 2;
-        if mid == 0 && array[mid] != key {
-            return None;
-        }
-        if array[mid] == key {
-            return Some(mid);
-        } else if array[mid] < key {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+
+        match array[mid].cmp(&key) {
+            std::cmp::Ordering::Equal => return Some(mid),
+            std::cmp::Ordering::Less => {
+                if mid == 0 {
+                    return None;
+                }
+                left = mid + 1
+            }
+            std::cmp::Ordering::Greater => {
+                if mid == 0 {
+                    return None;
+                }
+                right = mid - 1
+            }
         }
     }
     None
