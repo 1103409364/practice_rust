f60f11ea0fe2473a750053540290302dfbf35c57
Z7M <1103409364@qq.com>
2025-04-10 13:57:23 +0800

    feat: implement binary search algorithm in find function
    Signed-off-by: Z7M <1103409364@qq.com>

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,5 +1,22 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+    if array.is_empty() {
+        return None;
+    }
+    let mut left = 0;
+    let mut right = array.len() - 1;
+    while left <= right {
+        println!("left: {}, right: {}", left, right);
+        let mid = (left + right) / 2;
+        if mid == 0 && array[mid] != key {
+            return None;
+        }
+        if array[mid] == key {
+            return Some(mid);
+        } else if array[mid] < key {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    None
 }
